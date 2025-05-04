0645928d4e0728f0512adcf8b95ae241c588ff13
Pedro-Barbosa1 <pedro.pbarbosa@upe.br>
2025-05-04 16:50:20 -0300

    LogAnalyser.java

---
+++ src/main/java/br/upe/logsanalyser/model/LogAnalyser.java
@@ -0,0 +1,21 @@
+package br.upe.loganalyser.model;
+
+import java.util.List;
+
+public class LogAnalyser{
+  public void analisarRecursosGrande(List<LogEntry> logs){
+
+  }
+
+  public void analisarNaoRespondidos(List<LogEntry> logs){
+
+  }
+
+  public void calcularPercentualPorSO(List<LogEntry> logs){
+
+  }
+
+  public void mediaRequisicoesPOST(List<LogEntry> logs){
+
+  }
+}
